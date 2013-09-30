acb3c9cb8561432363ba8c0e4e66e14bdd41dd3c
Mrunal Nargunde <mrunalnargunde@gmail.com>
2013-09-30 18:45:26 -0400

    Removed errors from DiskLruCache

--- src/com/example/util/DiskLruCache.java
+++ src/com/example/util/DiskLruCache.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.example.android.bitmapfun.util;
+package com.example.util;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedWriter;
